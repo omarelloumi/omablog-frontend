7bfac600b9da78459949efd4438a2e076e9aec33
Omar Elloumi <omarelloumi842@yahoo.fr>
2025-08-29 08:41:49 +0200

    Configure routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import NavBar from "@/components/blocks/NavBar.tsx";
-import Footer from "@/components/blocks/Footer.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import AppLayout from "@/components/layouts/AppLayout.tsx";
+import HomePage from "@/pages/HomePage.tsx";
+import ProfilePage from "@/pages/ProfilePage.tsx";
+import BlogDetailPage from "@/pages/BlogDetailPage.tsx";
 
 
 function App() {
   return (
-      <>
-          <NavBar />
-
-          <Footer />
-      </>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<AppLayout />}>
+                      <Route index element={<HomePage />} />
+                      <Route path="detail" element={<BlogDetailPage banner={"banner.jpg"} />} />
+                      <Route path="profile" element={<ProfilePage />} />
+                  </Route>
+              </Routes>
+          </BrowserRouter>
   )
 }
 
--- src/components/cards/BlogCard.tsx
+++ src/components/cards/BlogCard.tsx
@@ -1,12 +1,13 @@
 import Badge from "@/components/ui/Badge.tsx";
-import CardFooter from "@/components/ui/CardFooter.tsx";
+import Author from "@/components/ui/Author.tsx";
+import {Link} from "react-router-dom";
 
 type Props = {
     thumbnail: string;
 };
 const BlogCard = ({ thumbnail }: Props) => {
     return (
-        <div className="px-3 py-3 rounded-md w-[300px] h-auto flex flex-col gap-4 dark:border-gray-800 border shadow-lg">
+        <Link to={"/detail"} className="px-3 py-3 rounded-md w-[300px] h-auto flex flex-col gap-4 dark:border-gray-800 border shadow-lg transition-shadow duration-300 hover:scale-105">
             <div className="w-full h-[200px] border rounded-md overflow-hidden">
                 <img
                     src={thumbnail}
@@ -18,8 +19,9 @@ const BlogCard = ({ thumbnail }: Props) => {
             <h3 className="font-semibold  leading-normal text-[#181A2A] mb-0 dark:text-white">
                 Build and Ecommerce Web App with Django and React
             </h3>
-            <CardFooter pic={"profile.png"}/>
-        </div>
+            <Author pic={"profile.png"}/>
+
+        </Link>
     );
 };
 export default BlogCard;
